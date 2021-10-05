88e645dc3e676cd1f57755d392a88fcb70540dee
Lawrence Liu <smartlitchi@gmail.com>
2021-10-05 10:45:01 +0800

    Reproduce can not reload using undertow for grails-audit-logging-plugin/223

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,3 @@
+// Added by the Audit-Logging plugin:
+grails.plugin.auditLog.auditDomainClassName = 'tech.muyan.audit.AuditTrail'
+grails.plugin.auditLog.excluded = ['version', 'lastUpdated', 'lastUpdatedBy', 'dateCreated', 'createdBy']
